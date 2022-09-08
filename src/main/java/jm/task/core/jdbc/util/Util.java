2b5ca4111f20eeec3980b40edb1d15ebc3293ef5
remix1or <remixtor@gmail.com>
2022-09-08 18:41:49 +0300

    commit result

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,20 +9,12 @@ public class Util {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "admin";
-    private static Connection connection;
 
-    public static Connection getConnection() {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    public static Statement getStatement(Connection connection) throws SQLException {
+        return connection.createStatement();
 
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
     }
 }
